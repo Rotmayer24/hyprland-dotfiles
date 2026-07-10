e4d959b0c6b9fb53e10ef8155ffcc49c4d6111b6
Rotmayer24 <demkovychvovan@gmail.com>
2026-07-10 11:33:24 +0200

    Update monitor configuration

--- config/hypr/modules/monitors.lua
+++ config/hypr/modules/monitors.lua
@@ -1,13 +1,15 @@
 hl.monitor({
-	output = "DP-1",
-	mode = "1336x768@59.97",
-	position = "0x0",
-	scale = 1,
+    output = "eDP-1",
+    disabled = false,
+    mode = "1366x768@59.97Hz",
+    position = "100x433",
+    scale = 1,
+    cm = "srgb",
 })
 
 hl.monitor({
-	output = "HDMI-A-1",
-	mode = "1600x1201@60",
-	position = "0x-768",
-	scale = 1,
+    output = "HDMI-A-1",
+    mode = "1600x1201@60",
+    position = "0x-768",
+    scale = 1,
 })
